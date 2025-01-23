3953e1b3d66511ab01a5b33d3b5e74bc9c0f6934
davidgmolnar <92262848+davidgmolnar@users.noreply.github.com>
2025-01-23 14:07:17 +0100

    Updated pytest

--- pytest/pytest.py
+++ pytest/pytest.py
@@ -1,4 +1,4 @@
-from udpcanpy import NetworkHandler
+from udpcanpy import NetworkHandler, RemoteControl
 
 nh = NetworkHandler()
 res = nh.parse("/home/davidgmolnar/Documents/COM-2024/COM-2024-DBC/comms.dbc")
@@ -14,15 +14,36 @@ if(res != 0):
     print(f"Start {res}")
 
 remote = nh.getRemoteControl()
-remdata = remote.access()
-remdata = remote.access()
 
-remdata.left_trigger = 10
-remdata.right_trigger = 10
+data = RemoteControl()
 
-remote.update(remdata)
-remdata2 = remote.access()
-remdata2 = remote.access() # TODO fix this, when CAN_E_NOT_UPDATED we get memory garbage
+while True:
+    res = remote.access(data)
+    if res == 0:
+        print(f"=================\n\
+              LB: {data.l_bottom}\n\
+              LT: {data.l_top}\n\
+              LR: {data.l_right}\n\
+              LL: {data.l_left}\n\
+              RB: {data.r_bottom}\n\
+              RT: {data.r_top}\n\
+              RR: {data.r_right}\n\
+              RL: {data.r_left}\n\
+              LS: {data.l_shoulder}\n\
+              RS: {data.r_shoulder}\n\
+              LTrigger: {data.left_trigger}\n\
+              RTrigger: {data.right_trigger}\n\
+              ThumbLX: {data.thumb_left_x}\n\
+              ThumbLY: {data.thumb_left_y}\n\
+              ThumbRX: {data.thumb_right_x}\n\
+              ThumbRY: {data.thumb_right_y}\n\
+                ")
 
-i = 0
-print(i)
+    if data.e_stop:
+        break
+
+res = nh.stop()
+if(res != 0):
+    print(f"Stop {res}")
+
+print("Adiós")
