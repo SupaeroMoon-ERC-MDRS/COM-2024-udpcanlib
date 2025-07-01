d299a32c41e4b59a0d602bf9d298f3aad5c5ad19
davidgmolnar <92262848+davidgmolnar@users.noreply.github.com>
2025-07-01 16:06:42 +0000

    Debug prints

--- modules/can/src/candatabase.cpp
+++ modules/can/src/candatabase.cpp
@@ -61,6 +61,7 @@ uint32_t CanDatabase::parse(const std::string& fn){
 
         CanMessageDesc desc;
         CAN_E_FW_IF_ERR(desc.parse(in, eof))
+        std::cout << "Created message with id " << (uint16_t)desc.id << std::endl;
         messages[desc.id] = desc;
     }
 
@@ -108,7 +109,7 @@ uint32_t CanDatabase::encode(const uint8_t id, const std::map<std::string, std::
     std::set<std::string> msg_keys;
     std::set<std::string> in_keys;
 
-    std::cout << "1" << std::endl;
+    std::cout << "1 " << (uint16_t)id << std::endl;
     messages.at(id).getSignalNames(msg_keys);
     std::cout << "2" << std::endl;
     for(const std::pair<std::string, std::any> p : in){
